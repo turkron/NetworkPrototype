5d316c6ea3f0c9f813519aa00cb78a9da5a32ab5
turkron <nathandkendall@hotmail.com>
2015-05-29 19:44:00 +0100

    uploading file from network project to github

---
+++ Control.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI() {
+if(GUI.Button(new Rect(20,100,50,50),"up"))
+{
+GameObject.Find("Player(Clone)").transform.position = new Vector3(0,5,0);
+}
+}
---
+++ Instantiate.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+var SpaceCraft : Transform;
+function OnNetworkLoadedLevel () {
+// Instantiating SpaceCraft when Network is loaded
+Network.Instantiate(SpaceCraft, transform.position, transform.rotation, 0);
+}
+
+
+function OnPlayerDisconnected (player : NetworkPlayer) {
+Network.RemoveRPCs(player, 0);
+Network.DestroyPlayerObjects(player);
+}
